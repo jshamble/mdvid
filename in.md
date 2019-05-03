85e7890c2c62f4fdbe12ebd6d394469a3b7dc705
Jonathan Shamblen <jshamble.js@gmail.com>
2019-05-03 12:16:40 -0700

    added check for youtube videos that were already processed, skips replacing those lines.

--- in.md
+++ in.md
@@ -1,5 +1,6 @@
-https://www.youtube.com/watch?v=5ZOAiSP1MGs
+[![Watch the video](https://img.youtube.com/vi/5ZOAiSP1MGs/sddefault.jpg)](http://www.youtube.com/watch?v=5ZOAiSP1MGs)
 
 https://www.youtube.com/watch?v=AVXejOoPECA
 
 https://www.youtube.com/watch?v=5897dMWJiSM&t=333s
+
--- mdvid.py
+++ mdvid.py
@@ -30,29 +30,32 @@ with codecs.open(input_name, 'r', encoding='utf-8') as fi, \
 	for line in fi:
 		new_line = line # do_processing(line) # do your line processing here
 		if "http://www.youtube.com" in line or "https://www.youtube.com" in line:
-			youtube_url = re.search(youtube_regex, new_line)
-			if(youtube_url != None):
-				youtube_url_timestamp = youtube_url.groups()[1];
-				if (youtube_url_timestamp == None):
-					youtube_url_timestamp = ""
-				youtube_url = youtube_url.groups()[0]
-				new_line = re.sub(youtube_regex, '[![Watch the video](https://img.youtube.com/vi/' + youtube_url + '/sddefault.jpg)](' + youtube_begin_string + youtube_url + youtube_url_timestamp + ')', new_line)
-				
-				if(print_title or print_desc):
-					youtube_html = requests.get(youtube_begin_string + youtube_url) 
-					youtube = etree.HTML(youtube_html.text)
-				
-				if(print_title):
-					video_title = youtube.xpath("//span[@id='eow-title']/@title")
-					title_to_be_decoded = ''.join(video_title)
-					fo.write( (b'# ' + (title_to_be_decoded.encode('utf-8')) + b"\n").decode() )
-					# print( title_to_be_decoded.encode("utf-8") )
-				fo.write(new_line)
-				if(print_desc):
-					video_desc = youtube.xpath("//p[@id='eow-description']/text()")
-					desc_to_be_decoded = ''.join(video_desc)
-					fo.write( (b'# ' + (desc_to_be_decoded.encode('utf-8')) + b"\n").decode() )
-					# print(desc_to_be_decoded.encode("utf-8"))
+				youtube_url = re.search(youtube_regex, new_line)
+				if(youtube_url != None):
+					youtube_url_timestamp = youtube_url.groups()[1];
+					if (youtube_url_timestamp == None):
+						youtube_url_timestamp = ""
+					youtube_url = youtube_url.groups()[0]
+					
+					# updated it so that already processed videos with this program are unchanged.
+					if not "[![" in line:
+						new_line = re.sub(youtube_regex, '[![Watch the video](https://img.youtube.com/vi/' + youtube_url + '/sddefault.jpg)](' + youtube_begin_string + youtube_url + youtube_url_timestamp + ')', new_line)
+						
+					if(print_title or print_desc):
+						youtube_html = requests.get(youtube_begin_string + youtube_url) 
+						youtube = etree.HTML(youtube_html.text)
+					
+					if(print_title):
+						video_title = youtube.xpath("//span[@id='eow-title']/@title")
+						title_to_be_decoded = ''.join(video_title)
+						fo.write( (b'# ' + (title_to_be_decoded.encode('utf-8')) + b"\n").decode() )
+						# print( title_to_be_decoded.encode("utf-8") )
+					fo.write(new_line)
+					if(print_desc):
+						video_desc = youtube.xpath("//p[@id='eow-description']/text()")
+						desc_to_be_decoded = ''.join(video_desc)
+						fo.write( (b'# ' + (desc_to_be_decoded.encode('utf-8')) + b"\n").decode() )
+						# print(desc_to_be_decoded.encode("utf-8"))
 		
 os.remove(output_name) # remove output
 os.rename(tmp_name, output_name) # rename temp to output name
